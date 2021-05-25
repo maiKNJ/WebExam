9bec2198d3ef0f9d2c832646736983526d009705
maiKNJ <43847780+maiKNJ@users.noreply.github.com>
2021-05-26 00:17:30 +0200

    Profile page image slide. start on edit page

--- ExamProject/js/slideshowAbout.js
+++ ExamProject/js/slideshowAbout.js
@@ -1,33 +1,80 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+$(".next").on('click', function(){
+  var currentImg = $(".active");
+  var nextImg = currentImg.next();
 
-function plusSlider(n){
+  if(nextImg.length){
+    currentImg.removeClass('active').css('z-index', -10);
+    nextImg.addClass('active').css('z-index', 10);
+  }
+});
+
+$(".prev").on('click', function(){
+  var currentImg = $(".active");
+  var prevImage = currentImg.prev();
+
+  if(prevImage.length){
+    currentImg.removeClass('active').css('z-index', -10);
+    prevImage.addClass('active').css('z-index', 10);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var slideIndex = 1;
+showSlides();
+
+function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n){
+function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n){
   var i;
-  var slides = $(".myslides");
-  var dots = $(".demo");
-  var captionText = $("#caption");
-
+  var slides = $(".mySlides");
+  var dots = $(".dot");
   if(n > slides.length){
     slideIndex = 1;
   }
   if(n < 1){
     slideIndex = slides.length;
   }
-  for (i = 0; i>slides.length; i++){
-    slides[i].style.display = "none";
+  for (i = 0; i<slides.length; i++){
+    $(".mySlides").each(function (i){
+      $(this).css('display', 'none');
+    });
+    //$(this, ".mySlides").css('style', 'none');
   }
-  for (i = 0; i<dots.length; i++){
-    dots[i].className = dots[i].className.replace("active", "");
+  for ( i = 0; i < dots.length; i++) {
+    $(".dot").each(function (i){
+      $(this).attr('dot', 'active');
+    });
+    //$(this, ".dot").className = $(this, ".dot").className.replace("active");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += "active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+  slideIndex -= 1;
+  $(".mySlides").each(function (slideIndex){
+    $(this).css('display', 'block');
+  });
+  $(".dot").each(function (slideIndex) {
+    $(this).addClass('active');
+  });
+  //slides[slideIndex - 1].css('display', 'block');
+  //dots[slideIndex-1].className += "active";
+}*/
