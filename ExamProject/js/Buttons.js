3b80a4028f6e7d2507e636b0520bac81d73fde9f
FriendlyFisting <43848000+JulieTofte@users.noreply.github.com>
2021-05-26 22:06:30 +0200

    sponsors
    closer

--- ExamProject/js/Buttons.js
+++ ExamProject/js/Buttons.js
@@ -1,10 +1,19 @@
-function loadDoc() {
+function sponsor1() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("demo").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "demo_get.asp", true);
+  xhttp.open("GET", "sponsor1.txt", true);
   xhttp.send();
+}
+
+function sponsor2() {
+  var x = document.getElementById("spons1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
