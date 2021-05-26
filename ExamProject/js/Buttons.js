92418c858bb7a9f750321d477e272af24df30129
FriendlyFisting <43848000+JulieTofte@users.noreply.github.com>
2021-05-26 23:45:37 +0200

    Sponsor
    a very sketchy sketch, but it works!

--- ExamProject/js/Buttons.js
+++ ExamProject/js/Buttons.js
@@ -1,19 +1,32 @@
-function sponsor1() {
+function SponsorText1() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
+      document.getElementById("sponsorText").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", "sponsor1.txt", true);
   xhttp.send();
+  }
+
+function SponsorText2() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("sponsorText").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "sponsor2.txt", true);
+  xhttp.send();
+}
+function SponsorText3() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("sponsorText").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "sponsor3.txt", true);
+  xhttp.send();
 }
 
-function sponsor2() {
-  var x = document.getElementById("spons1");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
